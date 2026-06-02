e7e40520a482f446d72ddf596863b0e24a13eb54
Nishant Aryan <btech10443.25@bitmesra.ac.in>
2026-06-03 04:16:44 +0530

    Replace Script component with standard HTML script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-
-import Script from 'next/script';
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
@@ -23,37 +21,17 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full antialiased dark bg-slate-950" suppressHydrationWarning>
       <head>
-        {/* PopAds Anti-Adblock Script Integration */}
-        <Script
-          id="popads-script"
-          strategy="afterInteractive"
+        {/* PopAds Anti-Adblock Script - Standard HTML for Troubleshooter Verification */}
+        <script 
+          type="text/javascript" 
+          data-cfasync="false"
           dangerouslySetInnerHTML={{
             __html: `
-              (function(){
-                var u=window,
-                    z="b479e157b324167e9abb6eeb2a7c5511",
-                    j=[["siteId",5301392],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],
-                    k=["d3d3LmFudGlhZGJsb2Nrc3lzdGVtcy5jb20vWmVVcFlvL3F1c2VyaW5mby5taW4uanM=","ZDNjb2Q4MHRobjdxbmQuY2xvdWRmcm9udC5uZXQvR2RGb28vdlgvdmx1bnIubWluLmNzcw=="],
-                    f=-1,d,r,
-                    p=function(){
-                      clearTimeout(r);f++;
-                      if(k[f]&&!(1806355132000<(new Date).getTime()&&1<f)){
-                        d=u.document.createElement("script");
-                        d.type="text/javascript";
-                        d.async=!0;
-                        var h=u.document.getElementsByTagName("script")[0];
-                        d.src="https://"+atob(k[f]);
-                        d.crossOrigin="anonymous";
-                        d.onerror=p;
-                        d.onload=function(){clearTimeout(r);u[z.slice(0,16)+z.slice(0,16)]||p()};
-                        r=setTimeout(p,5E3);
-                        h.parentNode.insertBefore(d,h);
-                      }
-                    };
-                if(!u[z]){try{Object.freeze(u[z]=j)}catch(e){}p()}
-              })();
+/*<![CDATA[/* */
+(function(){var y=window,r="b479e157b324167e9abb6eeb2a7c5511",s=[["siteId",784*782-672+4688307],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],j=["d3d3LmFudGlhZGJsb2Nrc3lzdGVtcy5jb20vSC9idXNlcmluZm8ubWluLmpz","ZDNjb2Q4MHRobjdxbmQuY2xvdWRmcm9udC5uZXQveWQvRkwvc2x1bnIubWluLmNzcw==","d3d3LnNrc3prdXJubS5jb20vWUFFSi91dXNlcmluZm8ubWluLmpz","d3d3Lnppd3B5bmFzbi5jb20vU1B5L0JDL3psdW5yLm1pbi5jc3M="],x=-1,u,t,l=function(){clearTimeout(t);x++;if(j[x]&&!(1806359996000<(new Date).getTime()&&1<x)){u=y.document.createElement("script");u.type="text/javascript";u.async=!0;var w=y.document.getElementsByTagName("script")[0];u.src="https://"+atob(j[x]);u.crossOrigin="anonymous";u.onerror=l;u.onload=function(){clearTimeout(t);y[r.slice(0,16)+r.slice(0,16)]||l()};t=setTimeout(l,5E3);w.parentNode.insertBefore(u,w)}};if(!y[r]){try{Object.freeze(y[r]=s)}catch(e){}l()}})();
+/*]]>/* */
             `,
-          }}
+          }} 
         />
       </head>
       <body className="min-h-full flex flex-col bg-slate-950 text-white overflow-x-hidden">
